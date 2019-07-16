5f39c37fbb244dbc20e889321fec26b146ec0345
Donnie <dc23910@gmail.com>
2019-07-16 08:44:18 -0600

    formatted renderer.js

--- renderer.js
+++ renderer.js
@@ -1,4 +1,4 @@
-// Daniel Castillo 
+// Daniel Castillo formatted by Donald Collett
 // Leaflet Map in Electron
 
 var app = {
@@ -8,183 +8,199 @@ var app = {
     j             : 0,
     alllatlongs   : [],
     directions    : [],
-    foundLatLons  : [] };
+    foundLatLons  : [],
+};
 
 ///////////////////////////////////////////////////
 
-app.ws.onopen = function(event){
-console.log('connected')
+app.ws.onopen = function(event)
+{
+        console.log('connected')
 }
 
-app.ws.onmessage = function(event) { 
+app.ws.onmessage = function(event) 
+{ 
+        console.log("WebSocket message received");
+        app.cleanData.push(event.data);//get and concatenate the data
 
-app.j++
+        app.j++
 
-console.log("WebSocket message received");
-app.cleanData.push(event.data);//get and concatenate the data
+        if (app.cleanData.length > 1)
+        { 
+                app.retGPS = getData(app.cleanData, app.j);      // Analize the data and plot
 
+                // Leaflet Map Setup
+                map = new L.map('map',{center: [app.retGPS[0], app.retGPS[1]], zoom:15});
 
-if (app.cleanData.length > 1){ 
-app.retGPS = getData(app.cleanData, app.j);      // Analize the data and plot
-
-// Leaflet Map Setup
-var map = L.map('map',{center: [app.retGPS[0], app.retGPS[1]], zoom:15});
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-maxZoom: 50,
-id: 'mapbox.streets',
-accessToken: 'pk.eyJ1IjoiZGFub3JvOTYiLCJhIjoiY2p4ZGh4Zjh1MGViZzNubWY4dTRnbndpYiJ9.RlCJaOdgY9VQusXRfICljw'
-}).addTo(map);
+                L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', 
+                {
+                        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                        maxZoom: 50,
+                        id: 'mapbox.streets',
+                        accessToken: 'pk.eyJ1IjoiZGFub3JvOTYiLCJhIjoiY2p4ZGh4Zjh1MGViZzNubWY4dTRnbndpYiJ9.RlCJaOdgY9VQusXRfICljw'
+                }).addTo(map);
 
 
-for (var i = 0; i = app.j; i++){
+                for (var i = 0; i = app.alllatlongs.length; i++)
+                {
+                        console.log(app.alllatlongs)
+                        var latlngs = [];
 
-latlngs = app.alllatlongs[i];
-// loop to put all markers and polylines
-// Here go the markers
+                        for ( var j = 0; j < app.alllatlongs.length; j++)
+                        {
+                                for ( var k = 0; k< app.alllatlongs[j].length; k++)
+                                {
+                                        latlngs.push(app.alllatlongs[j][k]);
+                                }
+                        }
+                        console.log(latlngs);
+                        // loop to put all markers and polylines
+                        // Here go the markers
 
-markers(latlngs);
+                        markers(latlngs);
 
-// Here go the polylines
+                        // Here go the polylines
 
-polyline(latlngs);
+                        polyline(latlngs);
 
-// Putting all of it into the tables
+                        // Putting all of it into the tables
 
-document.getElementById('gpsData')
-        .appendChild(populateTable(null, 3, app.j, app.alllatlongs)); 
+                        document.getElementById('gpsData')
+                                .appendChild(populateTable(null, 3, app.j, app.alllatlongs)); 
 
-document.getElementById('directionData')
-        .appendChild(populateTable(null, 3, app.j, app.direcDat));
+                        document.getElementById('directionData')
+                                .appendChild(populateTable(null, 3, app.j, app.direcDat));
 
+                }
         }
-    }
 }
 
-app.ws.onclose = function(event){
-
-console.log('connection closed')
+app.ws.onclose = function(event)
+{
+        console.log('connection closed')
 }
 
-function req(){
-console.log("sent")
-app.ws.send("0")
-// console.log("after")
+function req()
+{
+        console.log("sent")
+        app.ws.send("0")
+        // console.log("after")
 }
 
 
 //////////////////////////////////////////////////////////////////////////////////
 
-function getData(cleanData, j){
-
-var cleanData1 = cleanData[j-2].split(",");
-var cleanData2 = cleanData[1].split(",");
-
-
-var  gpsLat = parseFloat(cleanData1[0]); // at 0
-var  gpsLong = parseFloat(cleanData1[1]); // at 1
-
-var lat = parseFloat(cleanData2[0]); // at 0
-var long = parseFloat(cleanData2[1]); // at 1
+function getData(cleanData, j)
+{
+        var cleanData1 = cleanData[j-2].split(",");
+        var cleanData2 = cleanData[1].split(",");
 
 
-var angleTru1 = parseFloat(cleanData1[6]); // at 10
-var angleTru2 = parseFloat(cleanData2[6]);  // at 10
+        var  gpsLat = parseFloat(cleanData1[0]); // at 0
+        var  gpsLong = parseFloat(cleanData1[1]); // at 1
 
-var lineArray = [];
-var angleArray = [];
+        var lat = parseFloat(cleanData2[0]); // at 0
+        var long = parseFloat(cleanData2[1]); // at 1
 
-// Put both the angle variables into an array for later use
-angleArray.push(math.tan(math.unit(angleTru1, 'deg')));
-angleArray.push(math.tan(math.unit(angleTru2, 'deg')));
 
-// Start analysis for position of thrid GPS point
+        var angleTru1 = parseFloat(cleanData1[6]); // at 10
+        var angleTru2 = parseFloat(cleanData2[6]);  // at 10
 
-lineArray.push(angleArray[0]*(gpsLong-long) + (gpsLat-lat));
+        var lineArray = [];
+        var angleArray = [];
 
-var a = [[angleArray[0], -1], [angleArray[1], -1]];
-var b = [[lineArray[0]], [0]];
-var coords = math.usolve(a, b);
+        // Put both the angle variables into an array for later use
+        angleArray.push(math.tan(math.unit(angleTru1, 'deg')));
+        angleArray.push(math.tan(math.unit(angleTru2, 'deg')));
 
-var cor0 = coords[0];
-var cor1 = coords[1];
+        // Start analysis for position of thrid GPS point
 
-var latNew = math.sum([lat, cor1]);
-var longNew = math.sum([long, cor0]);
+        lineArray.push(angleArray[0]*(gpsLong-long) + (gpsLat-lat));
 
-// Variable Setup for plot
+        var a = [[angleArray[0], -1], [angleArray[1], -1]];
+        var b = [[lineArray[0]], [0]];
+        var coords = math.usolve(a, b);
 
-var lat1  = lat;
-var lon1  = long;
-var lat2  = gpsLat;
-var lon2  = gpsLong;
-var latNu = latNew;
-var lonNu = longNew;
+        var cor0 = coords[0];
+        var cor1 = coords[1];
 
-// create a red polyline from an array of LatLng points
-var latlngs = [lat1, lon1, latNu, lonNu, lat2, lon2];
+        var latNew = math.sum([lat, cor1]);
+        var longNew = math.sum([long, cor0]);
 
-app.alllatlongs.push(latlngs);
+        // Variable Setup for plot
 
-nu = [latNu, lonNu]
+        var lat1  = lat;
+        var lon1  = long;
+        var lat2  = gpsLat;
+        var lon2  = gpsLong;
+        var latNu = latNew;
+        var lonNu = longNew;
 
-app.foundLatLons.push(nu);
+        // create a red polyline from an array of LatLng points
+        var latlngs = [lat1, lon1, latNu, lonNu, lat2, lon2];
 
-console.log(latlngs);
+        app.alllatlongs.push(latlngs);
 
-return latlngs
+        console.log(app.alllatlongs)
 
+        nu = [latNu, lonNu]
+
+        app.foundLatLons.push(nu);
+
+        //console.log(latlngs);
+
+        return latlngs
 }
 
 ////////////////////////////////////////////////////////////////
 
 // Function for adding elements to table dynamically
 
-function populateTable(table, rows, cells, content) {
-var is_func = (typeof content === 'function');
-if (!table) table = document.createElement('table');
-for (var i = 0; i < rows; ++i) {
-var row = document.createElement('tr');
-for (var j = 0; j < cells; ++j) {
-    row.appendChild(document.createElement('td'));
-    var text = !is_func ? (content + '') : content(table, i, j);
-    row.cells[j].appendChild(document.createTextNode(text));
-}
-table.appendChild(row);
-}
-return table;
-}
-
-////////////////////////////////////////////////////////////////
-
-
-function markers(latlngs){   
-
-lat1  = latlngs[0];
-lon1  = latlngs[1]; 
-latNu = latlngs[2];  
-lonNu = latlngs[3]; 
-lat2  = latlngs[4];
-lon2  = latlngs[5];
-
-// Setting the markers 
-L.marker([lat1, lon1]).addTo(map); // marker 1
-L.marker([lat2, lon2]).addTo(map); // marker 2
-L.marker([latNu, lonNu]).addTo(map); // marker 3
-
+function populateTable(table, rows, cells, content) 
+{
+        var is_func = (typeof content === 'function');
+        if (!table) table = document.createElement('table');
+                for (var i = 0; i < rows; ++i) 
+                {
+                        var row = document.createElement('tr');
+                        for (var j = 0; j < cells; ++j) 
+                        {
+                                row.appendChild(document.createElement('td'));
+                                var text = !is_func ? (content + '') : content(table, i, j);
+                                row.cells[j].appendChild(document.createTextNode(text));
+                        }
+                        table.appendChild(row);
+                }
+        return table;
 }
 
 ////////////////////////////////////////////////////////////////
 
-function polyline(latlngs){
 
-latlngs = [[latlngs[0], latlngs[1]], [latlngs[2], latlngs[3]], [latlngs[4], latlngs[5]]];
+function markers(latlngs)
+{   
+        var lat1  = latlngs[0];
+        var lon1  = latlngs[1]; 
+        var latNu = latlngs[2];  
+        var lonNu = latlngs[3]; 
+        var lat2  = latlngs[4];
+        var lon2  = latlngs[5];
 
-// Creating the line
-L.polyline(latlngs, {color: 'red'}).addTo(map);
+        // Setting the markers 
+        L.marker([lat1, lon1]).addTo(map); // marker 1
+        L.marker([lat2, lon2]).addTo(map); // marker 2
+        L.marker([latNu, lonNu]).addTo(map); // marker 3
+}
 
-// Getting the map to shoe exactly the area where the points are
-map.fitBounds(polyline.getBounds());
+////////////////////////////////////////////////////////////////
 
+function polyline(latlngs)
+{
+        latlngs = [[latlngs[0], latlngs[1]], [latlngs[2], latlngs[3]], [latlngs[4], latlngs[5]]];
+
+        // Creating the line
+        L.polyline(latlngs, {color: 'red'}).addTo(map);
+
+        // Getting the map to shoe exactly the area where the points are
+        //map.fitBounds(polyline.getBounds());
 }
