2ffc084d9f0abdaff99a2217361717bc30acbf90
Donnie <dc23910@gmail.com>
2019-07-16 12:54:03 -0600

    renderer formall^Cknown as broke

--- renderer.js
+++ renderer.js
@@ -127,9 +127,11 @@ function passData(){
        document.getElementById('mymap').appendChild(mapping());
 
 }
+
 function mapping(){
 
-        app.map.setView([app.retGPS[0], app.retGPS[1]])        
+        app.map.setView([app.retGPS[0], app.retGPS[1]], 15)        
+        
         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', 
         {
                 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
