fe265e60740c2463d755ee1168856fea45f04189
Ray <604960@bah.com>
2019-07-26 10:53:55 -0600

    working drone script

--- python/drone_goto.py
+++ python/drone_goto.py
@@ -13,8 +13,8 @@ Full documentation is provided at http://python.dronekit.io/examples/simple_goto
 # Park Waypoint 2 (Right side): 38.832584, -104.798622
 from __future__ import print_function
 import time
-from dronekit import connect, VehicleMode, LocationGlobalRelative
-
+from dronekit import connect, VehicleMode, LocationGlobalRelative, mavutil
+import math
 '''
 # Set up option parsing to get connection string
 import argparse
@@ -40,15 +40,30 @@ print('Awaiting Target Coordinates...')
 def drone_goto(com,baudrate, latitude, longitude):
 
     # Connect to the Vehicle
-    connection_string = str(com) + "," + str(baudrate)
+    if (com == '0'):
+        connection_string = '127.0.0.1:14550'
+    else:
+        connection_string = str(com) + "," + str(baudrate)
+        
     print('Connecting to vehicle on: %s' % connection_string)
     vehicle = connect(connection_string, wait_ready=True)
 
     #vehicle = connect(connection_string, wait_ready=False)
     #vehicle.wait_ready(True, raise_exception=False)
+    def get_distance_metres(aLocation1, aLocation2):
+        """
+        Returns the ground distance in metres between two LocationGlobal objects.
 
+        This method is an approximation, and will not be accurate over large distances and close to the 
+        earth's poles. It comes from the ArduPilot test code: 
+        https://github.com/diydrones/ardupilot/blob/master/Tools/autotest/common.py
+        """
+        dlat = aLocation2.lat - aLocation1.lat
+        dlong = aLocation2.lon - aLocation1.lon
+        return math.sqrt((dlat*dlat) + (dlong*dlong)) * 1.113195e5
 
     def arm_and_takeoff(aTargetAltitude):
+    
         """
         Arms vehicle and fly to aTargetAltitude.
         """
@@ -84,29 +99,59 @@ def drone_goto(com,baudrate, latitude, longitude):
             time.sleep(1)
 
 
-    arm_and_takeoff(10)
+    def fly_waypoint(dest_point):
+        print("Flying to waypoint..")
+        
+        vehicle.simple_goto(dest_point,groundspeed=10)
+        current_loc = vehicle.location.global_relative_frame
+        dist=get_distance_metres(current_loc, dest_point)
+        while (dist > 1):
+            time.sleep(0.25)
+            current_loc = vehicle.location.global_relative_frame
+            dist=get_distance_metres(current_loc, dest_point)
+        print("Arrived at waypoint!") 
+        
+    
+    
+    arm_and_takeoff(5)
 
     print("Set default/target airspeed to 3")
     vehicle.airspeed = 3
+    print("Set default RTL Altitude to same level")
+    vehicle.parameters['RTL_ALT'] = 10
 
     print("Going towards first point for 30 seconds ...")
-    point1 = LocationGlobalRelative(latitude, longitude, 20)
-    vehicle.simple_goto(point1)
+    point1 = LocationGlobalRelative(latitude, longitude, 5)
+    fly_waypoint(point1)
 
     # sleep so we can see the change in map
-    time.sleep(30)
+
+    #print("Setting vehicle to loiter")
+    #vehicle.mode = VehicleMode("LOITER")
+    time.sleep(5)
+    print("Dropping Payload...")
     
-    '''
-    print("Going towards second point for 30 seconds (groundspeed set to 10 m/s) ...")
-    point2 = LocationGlobalRelative(38.832584, -104.7986221, 20)
-    vehicle.simple_goto(point2, groundspeed=10)
-    '''
+    msg = vehicle.message_factory.command_long_encode(
+        0, 0,    # target_system, target_component
+        mavutil.mavlink.MAV_CMD_DO_DIGICAM_CONTROL, #command
+        1,          # confirmation
+        0,          # param 1, yaw in degrees
+        0,          # param 2, yaw speed deg/s
+        0,          # param 3, direction -1 ccw, 1 cw
+        0,          # param 4, relative offset 1, absolute angle 0
+        1, 0, 0)    # param 5 ~ 7 not used
+        
+    vehicle.send_mavlink(msg)
     
-    # sleep so we can see the change in map
-    #time.sleep(30)
+    time.sleep(5)
+
 
     print("Returning to Launch")
     vehicle.mode = VehicleMode("RTL")
+    
+    while (vehicle.location.global_relative_frame.alt > 1):
+        time.sleep(0.25)
+    print("Landed.");
 
     # Close vehicle object before exiting script
     print("Close vehicle object")
